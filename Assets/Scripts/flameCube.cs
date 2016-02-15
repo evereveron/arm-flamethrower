f7dbf34290d9d76317b5984efbf5a190aeb24a32
conscou <scoullisconstantina@gmail.com>
2016-02-15 16:34:22 -0500

    cube can be lit up

---
+++ Assets/Scripts/flameCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flameCube : MonoBehaviour {
+
+	public new ParticleSystem cubeOnFire;
+
+	// Use this for initialization
+	void Start () {
+		cubeOnFire.Stop ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.name == "Fire") {
+			cubeOnFire.Play();
+		}
+	}
+}
--- Assets/Scripts/flamethrower.cs
+++ Assets/Scripts/flamethrower.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class flamethrower : MonoBehaviour {
 	
-	public ParticleSystem flames;
+	public new ParticleSystem flames;
 	
 	// Use this for initialization
 	void Start () {
 		flames.emissionRate = 0.0f;
-		//GameObject.Find ("FlameCollider").GetComponent (Collider).enabled = false;
+		GameObject.Find("FlameCollider").GetComponent<Collider>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			flames.emissionRate = 1000.0f;
+			GameObject.Find("FlameCollider").GetComponent<Collider>().enabled = true;
+
 		} else {
 			flames.emissionRate = 0.0f;
+			GameObject.Find("FlameCollider").GetComponent<Collider>().enabled = false;
+
 		}
 	}
+
 }
